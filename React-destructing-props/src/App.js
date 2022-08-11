832dc72faa40ab934650c6eaaa6fb19643d902bc
Satria Wibowo <extencezone02@gmail.com>
2022-08-11 17:06:33 +0700

    up

---
+++ React-destructing-props/src/App.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+
+function AdditionProps(props) {
+  return (
+    <div>
+      <p>{props.name}</p>
+      <p>{props.description}</p>
+      <p>{props.price}</p>
+    </div>
+  );
+}
+function AdditionDestruction({ name, description, price }) {
+  return (
+    <div>
+      <p>{name}</p>
+      <p>{description}</p>
+      <p>{price}</p>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      {/*Props*/}
+      <p>
+        <AdditionProps
+          name={"myname props"}
+          description={"props"}
+          price={"50"}
+        />
+      </p>
+
+      {/*Destructuring Props*/}
+      <p>
+        <AdditionDestruction
+          name={"crees"}
+          description={"horay"}
+          price={"rp/50"}
+        />
+      </p>
+    </div>
+  );
+}
